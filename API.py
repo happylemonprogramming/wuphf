eab6d42d7449dcfd3220a15735cd1616b4602663
lemonknowsall <lemonknowsall@gmail.com>
2023-02-27 21:00:23 -0800

    list the imgurl and captions

--- API.py
+++ API.py
@@ -58,15 +58,21 @@ def status():
 def post():
   #Example JSON
     # JSON Body = {"name": "lemon", "caption": "it works! #yay #moneymaker", "imgurl":"aws.lemonissosmart.com/img"}
-
+    # JSON Body = {
+    #               'name': 'Lemon', 
+    #               'caption': "\n\nJust when I thought I've seen it all, my buddy calls me to come over and watch a dinosaur and lemon battle. This ain't no ordinary fight club! #DaveChappelle, \n\nI just got struck by lightning while making a lemonade... oh well, at least the drink will be extra strong this time! #LightningLemon #DaveChappelle", 
+    #               'imgurl': '//s3.amazonaws.com/appforest_uf/f1675978942446x792425085319267600/Lemon%20%26%20T-Rex.png, //s3.amazonaws.com/appforest_uf/f1675978976428x846564285372801800/Lightning%20%26%20Lemon%202.png', 
+    #               'meta_key': 'EAAH4DU8nw4UBAHB6u6PtNoKkeUWOEz19BKVdZB7YJTlJG7c504VZCTuMe40gu51TbOIWJyFE9SZAwatdZBSzEZCLNFbrZBPTBIlOF9b6ZC9VS3J2Bd2LPy9kKtT3pAWvl8wuzPjhsK7N1WyEeZCLIHDlbPziG48d2DlZBXYKzL5lE8LLZCSqdJPm46', 
+    #               'twitter_token': '1373830285607899136-pVXqgGJSIVdeu5rb4G3x6QyTDaDHrd', 
+    #               'twitter_secret': '5jtSSfgTYq3bx7bhaXMHaLfeWXPqBozlJYwN6xzUeaPsb'}
   # Variable loading for JSON
   json_data = request.get_json()
   print("API JSON data: ")
   print(json_data)
   dictionary_data = json.loads(json_data)
   name = dictionary_data['name']
-  caption = dictionary_data['caption']
-  imgurl = dictionary_data['imgurl']
+  caption = list(dictionary_data['caption'])
+  imgurl = list(dictionary_data['imgurl'])
   meta_key = dictionary_data['meta_key']
   twitter_token = dictionary_data['twitter_token']
   twitter_secret = dictionary_data['twitter_secret']
