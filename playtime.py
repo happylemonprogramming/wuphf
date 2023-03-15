f72ce9d5707b7104241122588d38a3cb17d5fc88
lemonknowsall <lemonknowsall@gmail.com>
2023-03-14 21:28:55 -0700

    test scheduler in wuphf using subprocess instead of bubble

--- playtime.py
+++ playtime.py
@@ -1,7 +1,9 @@
 import datetime
 import time
 
-target_date = datetime.datetime(2023, 3, 14, 21, 9)
+# Parse the input string into a datetime object
+target_date_str = "Mar 14, 2023 9:25 pm"
+target_date = datetime.datetime.strptime(target_date_str, "%b %d, %Y %I:%M %p")
 
 while datetime.datetime.now() < target_date:
     time.sleep(60)  # Wait for 1 minute
--- wuphf.py
+++ wuphf.py
@@ -18,47 +18,42 @@ i = sys.argv[11]
 
 
 print('DATETIME IS: ', youtube_key)
-start_time = time.time()
-# caption = 'This is a test'
-# imgurl = '//s3.amazonaws.com/appforest_uf/f1678073597751x317044281136753500/test.mp4'
 
-# Twitter Keys
-# twitter_token = os.environ.get('twitter_access_token')
-# twitter_secret = os.environ.get('twitter_access_token_secret')
 
-# Meta Keys
-# meta_key = os.environ.get('meta_user_access_token')
+import datetime
+import time
 
-# Youtube Keys
-# youtube_key = 'None'
+# Parse the input string into a datetime object
+target_date_str = youtube_key
+target_date = datetime.datetime.strptime(target_date_str, "%b %d, %Y %I:%M %p")
 
-# name = 'Lemon'
-# tonality = 'Positive'
-# influencer = 'spiderman'
-# tags = 'rocket, space, AI'
+while datetime.datetime.now() < target_date:
+    time.sleep(60)  # Wait for 1 minute
+else:
+    start_time = time.time()
 
-# Twitter submission
-Twitter = tweet(caption, imgurl, twitter_token, twitter_secret)
-twitter_time = time.time()-start_time
-relay1 = time.time()
-print('Twitter time: ', twitter_time)
-# Facebook submission
-Facebook = facebook_post(caption, imgurl, meta_key)
-facebook_time = time.time()-relay1
-relay2 = time.time()
-print('Facebook time: ', facebook_time)
-# Instagram submission
-Instagram = instagram_post(caption, imgurl, meta_key)
-instagram_time = time.time()-relay2
-relay3 = time.time()
-print('Instagram time: ', instagram_time)
-# # YouTube submission [NEED TO FIGURE OUT CALLBACK URI FROM CLIENT_SECRETS.JSON]
-# if imgurl.endswith('mp4'):
-#     YouTube = youtube_upload(imgurl, youtube_key, name, tonality, influencer, tags)
-#     youtube_time = time.time()-relay3
-#     print('YouTube time: ', youtube_time)
-total_time = time.time()-start_time
-print('Total time: ', total_time)
+    # Twitter submission
+    Twitter = tweet(caption, imgurl, twitter_token, twitter_secret)
+    twitter_time = time.time()-start_time
+    relay1 = time.time()
+    print('Twitter time: ', twitter_time)
+    # Facebook submission
+    Facebook = facebook_post(caption, imgurl, meta_key)
+    facebook_time = time.time()-relay1
+    relay2 = time.time()
+    print('Facebook time: ', facebook_time)
+    # Instagram submission
+    Instagram = instagram_post(caption, imgurl, meta_key)
+    instagram_time = time.time()-relay2
+    relay3 = time.time()
+    print('Instagram time: ', instagram_time)
+    # # YouTube submission [NEED TO FIGURE OUT CALLBACK URI FROM CLIENT_SECRETS.JSON]
+    # if imgurl.endswith('mp4'):
+    #     YouTube = youtube_upload(imgurl, youtube_key, name, tonality, influencer, tags)
+    #     youtube_time = time.time()-relay3
+    #     print('YouTube time: ', youtube_time)
+    total_time = time.time()-start_time
+    print('Total time: ', total_time)
 
 
 # # 30 second video test
