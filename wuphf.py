2ab33a2b34ba1439311eb20dd230458de5136b68
lemonknowsall <lemonknowsall@gmail.com>
2023-08-16 22:41:14 -0700

    suppressed twitter due to API changes and time measurements

--- wuphf.py
+++ wuphf.py
@@ -56,24 +56,24 @@ while datetime.datetime.now() < target_date:
 else:
     start_time = time.time()
     print('wuphf.py is running')
-    # Twitter submission
-    if twitter_secret or twitter_token != 'None':
-        Twitter = tweet(caption, imgurl, twitter_token, twitter_secret)
-        twitter_time = time.time()-start_time
-        relay1 = time.time()
-        print('Twitter time: ', twitter_time)
+    # # Twitter submission
+    # if twitter_secret or twitter_token != 'None':
+    #     Twitter = tweet(caption, imgurl, twitter_token, twitter_secret)
+    #     twitter_time = time.time()-start_time
+    #     relay1 = time.time()
+    #     print('Twitter time: ', twitter_time)
     # Facebook submission
     if facebook_key != 'None':
         Facebook = facebook_post(caption, imgurl, facebook_key)
-        facebook_time = time.time()-relay1
-        relay2 = time.time()
-        print('Facebook time: ', facebook_time)
+        # facebook_time = time.time()-relay1
+        # relay2 = time.time()
+        # print('Facebook time: ', facebook_time)
     if instagram_key != 'None':
         # Instagram submission
         Instagram = instagram_post(caption, imgurl, instagram_key)
-        instagram_time = time.time()-relay2
-        relay3 = time.time()
-        print('Instagram time: ', instagram_time)
+        # instagram_time = time.time()-relay2
+        # relay3 = time.time()
+        # print('Instagram time: ', instagram_time)
     # # YouTube submission [NEED TO FIGURE OUT CALLBACK URI FROM CLIENT_SECRETS.JSON]
     # if imgurl.endswith('mp4'):
     #     YouTube = youtube_upload(imgurl, youtube_key, name, tonality, influencer, tags)
